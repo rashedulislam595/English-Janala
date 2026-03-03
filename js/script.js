382fa5a184648a47cd184afbedfa0851530f6991
rashedulislam595 <rashedulislam956581@gmail.com>
2026-03-03 15:28:57 +0600

    create by dynamic vocabularies word card from API

--- js/script.js
+++ js/script.js
@@ -1,24 +1,64 @@
 const loadLevel = () => {
     fetch("https://openapi.programming-hero.com/api/levels/all")
-    .then(response => response.json())
-    .then(data => displayLevel(data.data))
+        .then(response => response.json())
+        .then(data => displayLevel(data.data))
 }
 
-const displayLevel =(lessons) =>{
+const loadWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayWord(data.data))
+}
+
+
+const displayLevel = (lessons) => {
     // get parent div
     const levelContainer = document.getElementById("lesson-container");
-    levelContainer.innerHTML ="";
-    
+    levelContainer.innerHTML = "";
+
     // get lesson item 
-    for(lesson of lessons ){
+    for (lesson of lessons) {
         // create child / lesson filed
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-            <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> lesson -${lesson.level_no}</button>
+            <button onclick="loadWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> lesson -${lesson.level_no}</button>
         `
         // append child
         levelContainer.append(btnDiv);
     }
+}
+
+const displayWord = (words) => {
+    const WordContainer = document.getElementById("word-container");
+    WordContainer.innerHTML = "";
+
+//     {
+//     "id": 38,
+//     "level": 3,
+//     "word": "Mourn",
+//     "meaning": "শোক প্রকাশ করা",
+//     "pronunciation": "মোর্ন"
+// }
+
+    words.forEach(word => {
+        console.log(word)
+        const card = document.createElement("div");
+        card.innerHTML = `
+            <div class="bg-white text-center pt-14 pb-10 px-3 rounded-xl ">
+                <h2 class="text-3xl font-bold mb-3">${word.word}</h2>
+                <p class="text-xl font-medium mb-6">Meaning /Pronounciation</p>
+                <h2 class="font-bangla text-3xl font-semibold text-[#58585c] mb-3">"${word.meaning} / ${word.pronunciation}"</h2>
+
+                <div class="flex justify-between items-center">
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+                </div>
+            </div>
+        `
+
+        WordContainer.append(card)
+    })
 }
 
 loadLevel();
